75021936344a9b5ecdf6d77aea614403e25a9c65
ouyo <ouyo@sunday.systems>
2017-01-17 22:33:54 +0900

    add

---
+++ python3/Tensorflow/01/12CNN.py
@@ -0,0 +1,172 @@
+# View more python tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+"""
+Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
+"""
+from __future__ import print_function
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+# number 1 to 10 data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
+    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
+    return result
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    # stride [1, x_movement, y_movement, 1]
+    # Must have strides[0] = strides[3] = 1
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    # stride [1, x_movement, y_movement, 1]
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 784]) # 28x28
+ys = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
+x_image = tf.reshape(xs, [-1, 28, 28, 1])
+# print(x_image.shape)  # [n_samples, 28,28,1]
+
+## conv1 layer ##
+W_conv1 = weight_variable([5,5, 1,32]) # patch 5x5, in size 1, out size 32
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
+h_pool1 = max_pool_2x2(h_conv1)                                         # output size 14x14x32
+
+## conv2 layer ##
+W_conv2 = weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
+h_pool2 = max_pool_2x2(h_conv2)                                         # output size 7x7x64
+
+## fc1 layer ##
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = bias_variable([1024])
+# [n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+## fc2 layer ##
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+
+# the error between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))       # loss
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+sess = tf.Session()
+# important step
+# tf.initialize_all_variables() no long valid from
+# 2017-03-02 if using tensorflow >= 0.12
+sess.run(tf.global_variables_initializer())
+
+for i in range(5000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
+    if i % 50 == 0:
+        print(compute_accuracy(
+            mnist.test.images, mnist.test.labels))
+        
+"""
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.labeled_tensor import batch
+
+#学习数据
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+#输出准确率
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs})#预测
+    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))#对比预测值与真实值
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
+    return result
+
+def weight_variable(shape):
+    inital = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(inital)
+    
+def bias_variable(shape):
+    inital = tf.constant(0.1, shape=shape)
+    return tf.Variable(inital)
+
+def conv2d(x, W):#卷积
+    #stride[1, x_movement, y_moverment, 1]
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME') # x输入值 W 权重 strides定义跨步长度 padding抽取模式 SAME or VALID
+
+def max_pool_2x2(x):#pool (max)
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 784])  # 28 x 28
+ys = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
+
+x_image = tf.reshape(xs, [-1, 28, 28, 1]) #-1 图片维度(图片数量 尤为未知设为-1) | 28 x 28 | 1 图片为黑白所以为1   彩色为3
+#print(x_image.shape)
+
+#conv1 layer
+W_conv1 = weight_variable([5, 5, 1, 32]) # 形状 5 x 5 | 输入 1 (黑白,彩色为3) | 输出32高度
+b_conv1 = bias_variable([32]) #输出32 既有32个偏向
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) #输出 28 x 28 x 32 (由于使用SAME形式抽出  x 与 y的跨度为1)
+h_pool1 = max_pool_2x2(h_conv1) #输出14 x 14 x 32 (x 与 y 的跨度为2)
+
+#conv2 layer
+W_conv2 = weight_variable([5, 5, 32, 64]) # 形状 5 x 5 | 输入32 | 输出64高度
+b_conv2 = bias_variable([64]) #输出64 既有64个偏向
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) #输出 14 x 14 x 74 (由于使用SAME形式抽出  x 与 y的跨度为1)
+h_pool2 = max_pool_2x2(h_conv2) #输出7 x 7 x 64 (x 与 y 的跨度为2)
+
+#func1 layer
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = bias_variable([1024])
+
+#卷积数据变平
+h_pool2_flar = tf.reshape(h_pool2, [-1, 7*7*64]) #[n_samples, 7, 7, 64] => [n_samples, 7 * 7 * 64]
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flar, W_fc1) + b_fc1)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+#func2 layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))  # loss
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
+    if i % 50 == 0:
+        print(compute_accuracy(
+            mnist.test.images, mnist.test.labels))
+"""
